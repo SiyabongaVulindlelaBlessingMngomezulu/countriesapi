269e1bae18810326e9929ef940db0882455d0ac0
Siyabonga Mngomezulu <siyabongavulindlelablessing18@gmail.com>
2022-04-17 23:56:01 +0200

    my assignmment submission

---
+++ Data/ICountryData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using palota_func_countries_assessment.Model;
+
+namespace palota_func_countries_assessment.Data
+{
+    public interface ICountryData
+    {
+        public Country GetACountry(string countryCode);
+        public List<Country> GetAllCountries();
+
+        public List<string> GetCountryBorders(string countryCode);
+
+        public List<Country> GetCountriesInContinent(string region);
+
+    }
+}
